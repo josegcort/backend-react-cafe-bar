e927edb75d7edc5951dce3bdfd96f966d7d21497
josegcort <josegcort@gmail.com>
2021-11-17 14:20:50 -0500

    first commit

---
+++ Controllers/EmpleadoController.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace APICafecito.Controllers
+{
+    [Route("api/nosotros/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase{
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public EmpleadoController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        //CONSULTA
+        [HttpGet]
+        public JsonResult Get(){
+            string query = @"
+			            SELECT id,
+                        nombre,
+                        apellido,
+                        cargo,
+                        foto,
+                        instagram,
+                        youtube,
+                        facebook,
+                        twitter
+                        FROM bd_cafecito.empleado;
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource)) {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon)) {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,16 @@
+﻿namespace APICafecito.Models
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string cargo { get; set; }
+        public string foto { get; set; }
+        public string instagram { get; set; }
+        public string youtube { get; set; }
+        public string facebook { get; set; }
+        public string twitter { get; set; }
+
+    }
+}
