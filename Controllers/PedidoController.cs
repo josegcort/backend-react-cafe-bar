5ca919188411f85b964c2be1237ddce0001e4121
josegcort <josegcort@gmail.com>
2021-11-26 17:29:57 -0500

    Pedido con estado

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,185 @@
+﻿using System.Data;
+using APICafecito.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace APICafecito.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public PedidoController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        //CONSULTA
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+			            SELECT id,
+                        nombreCliente,
+                        telefonoCliente,
+                        emailCliente,
+                        indicaciones,
+                        productosCompra,
+                        valorCompra,
+                        estado
+                        FROM pedido;
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        //CREACION
+        [HttpPost]
+        public JsonResult Post(Models.Pedido pedi)
+        {
+            string query = @"
+                        INSERT INTO pedido
+                        (nombreCliente,
+                        telefonoCliente,
+                        emailCliente,
+                        indicaciones,
+                        valorCompra,
+                        productosCompra,
+                        estado)
+                        VALUES
+                        (@PedidoNombreCliente,
+                        @PedidoTelefonoCliente,
+                        @PedidoEmailCliente,
+                        @PedidoIndicaciones,
+                        @PedidoValorCompra,
+                        @PedidoProductosCompra,
+                        @PedidoEstado);
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@PedidoId", pedi.id);
+                    myCommand.Parameters.AddWithValue("@PedidoNombreCliente", pedi.nombreCliente);
+                    myCommand.Parameters.AddWithValue("@PedidoTelefonoCliente", pedi.telefonoCliente  );
+                    myCommand.Parameters.AddWithValue("@PedidoEmailCliente", pedi.emailCliente  );
+                    myCommand.Parameters.AddWithValue("@PedidoIndicaciones", pedi.indicaciones);
+                    myCommand.Parameters.AddWithValue("@PedidoProductosCompra", pedi.productosCompra);
+                    myCommand.Parameters.AddWithValue("@PedidoValorCompra", pedi.valorCompra);
+                    myCommand.Parameters.AddWithValue("@PedidoEstado", pedi.estado);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        //Modificacion
+        [HttpPut]
+        public JsonResult Put(Pedido pedi)
+        {
+            string query = @"                       
+                        UPDATE pedido
+                        SET
+                        nombreCliente = @PedidoNombreCliente,
+                        telefonoCliente = @PedidoTelefonoCliente,
+                        emailCliente = @PedidoEmailCliente,
+                        indicaciones = @PedidoIndicaciones ,
+                        valorCompra = @PedidoValorCompra,
+                        productosCompra = @PedidoProductosCompra,
+                        estado = @PedidoEstado
+                        WHERE id = @PedidoId;
+
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@PedidoId", pedi.id);
+                    myCommand.Parameters.AddWithValue("@PedidoNombreCliente", pedi.nombreCliente);
+                    myCommand.Parameters.AddWithValue("@PedidoTelefonoCliente", pedi.telefonoCliente  );
+                    myCommand.Parameters.AddWithValue("@PedidoEmailCliente", pedi.emailCliente  );
+                    myCommand.Parameters.AddWithValue("@PedidoIndicaciones", pedi.indicaciones);
+                    myCommand.Parameters.AddWithValue("@PedidoProductosCompra", pedi.productosCompra);
+                    myCommand.Parameters.AddWithValue("@PedidoValorCompra", pedi.valorCompra);
+
+                    myCommand.Parameters.AddWithValue("@PedidoEstado", pedi.estado);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        //ELIMINACION
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                        DELETE FROM pedido 
+                        WHERE id=@PedidoId;
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@PedidoId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿namespace APICafecito.Models
+{
+    public class Pedido
+    {
+        public int id { get; set; }
+        public string nombreCliente { get; set; }
+        public string telefonoCliente { get; set; }
+        public string emailCliente { get; set; }
+        public string indicaciones { get; set; }
+        public string productosCompra { get; set; }
+        public double valorCompra { get; set; }
+        public string estado { get; set; }
+    }
+}
